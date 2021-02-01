0f4c7d96e2b5d683d1ebd140f1afe0f178d26d5e
A Greaves <gilhespy.anna@gmail.com>
2021-02-01 15:43:31 +0000

    Add code to return correct answer from calculateCorrectAnswer()

--- assets/js/script.js
+++ assets/js/script.js
@@ -42,7 +42,22 @@ function checkAnswer() {
 
 }
 
+/**
+ * Gets the operands (the numbers) and the operator (plus, minus etc)
+ * directly from the dom, and returns the correct answer.
+ */
 function calculateCorrectAnswer() {
+
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
 
