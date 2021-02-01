4ec40c0c1c1675e35151500c7c0bdeb21bc49643
A Greaves <gilhespy.anna@gmail.com>
2021-02-01 09:38:13 +0000

    Reset commit for new recording

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,31 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                runGame(gameType);
+                alert(`You clicked ${gameType}`);
             }
         })
     }
-
-    runGame("addition");
 })
 
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame(gameType) {
+function runGame() {
 
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
-
-    if (gameType === "addition") {
-        displayAdditionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type ${gameType}`);
-        throw `Unknown game type ${gameType}, aborting!`;
-    }
-
 }
 
 function checkAnswer() {
@@ -53,11 +43,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion(operand1, operand2) {
-
-    document.getElementById("operand1").textContent = operand1;
-    document.getElementById("operand2").textContent = operand2;
-    document.getElementById("operator").textContent = "+";
+function displayAdditionQuestion() {
 
 }
 
