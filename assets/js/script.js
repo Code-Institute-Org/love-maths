7f5fee2694be2d20c5108cdf43dec7c9b6f14776
A Greaves <gilhespy.anna@gmail.com>
2021-02-03 09:12:26 +0000

    Reverse code from previous lesson for new recording

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,10 +117,6 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
 
-    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
-    document.getElementById('operator').textContent = "-";
-
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
